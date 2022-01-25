99fc5cb8f2c0df8e2414b623f30e1d975b7d22d6
FalseIlyu <ilyulemail@gmail.com>
2022-01-25 22:04:11 +0100

    Default value for bwm

--- file_definitions/file_definition_bwm.py
+++ file_definitions/file_definition_bwm.py
@@ -5,6 +5,8 @@ from typing import List
 from glob import glob
 import struct
 
+from numpy import mat
+
 from file_definition_utilities import (
     read_float,
     read_int16,
@@ -23,11 +25,14 @@ class BWMFile:
     """
 
     def __init__(self, reader: BufferedReader = None):
-        if reader:
-            self.fileHeader = BWMHeader(reader)
-            self.modelHeader = LionheadModelHeader(reader)
+        self.fileHeader = BWMHeader(reader)
+        self.modelHeader = LionheadModelHeader(reader)
         self.materialDefinitions = [
             MaterialDefinition(reader)
+            for i in range(self.modelHeader.materialDefinitionCount)
+        ]
+        self.materialDefinitions = [
+            MaterialDefinition()
             for i in range(self.modelHeader.materialDefinitionCount)
         ]
         self.meshDescriptions = [
@@ -71,9 +76,7 @@ class BWMFile:
                 for i in range(self.modelHeader.modelCleaveCount)
             ]
 
-            return
-        else:
-            raise ValueError("Must have access to a valid binary reader")
+        return
 
     def metadataSize(self):
         size = 0x80
@@ -100,7 +103,8 @@ class BWMFile:
 
     def write(self, filepath:str):
         writer = open(filepath,"xb")
-
+        self.fileHeader.size = self.size()
+        self.fileHeader.metadataSize = self.metadataSize()
         self.fileHeader.write(writer)
         self.modelHeader.write(writer)
         for materialDefinition in self.materialDefinitions:
@@ -132,6 +136,7 @@ class BWMFile:
             for modelCleave in self.modelCleaves:
                 write_vector(writer, modelCleave, write_float)
 
+
 class BWMHeader:
     """
     '  Header for BWM files, contains identifier for the format
@@ -158,6 +163,12 @@ class BWMHeader:
             self.metadataSize = read_int32(reader)  # 0x34
             # 0x38 + metadataSize = vertexPointer
             return
+        else:
+            self.fileIdentifier = "LiOnHeAdMODEL"
+            self.size = 0
+            self.numberIdentifier = 0x2B00B1E5
+            self.version = 5
+            self.metadataSize = 0
 
     def write(self, writer:BufferedWriter = None):
         if writer:
@@ -201,6 +212,31 @@ class LionheadModelHeader:
             self.indexCount = read_int32(reader)  # 0xB4
             self.modelCleaveCount = 0
             return
+        else:
+            self.unknown1 = 0.0
+            self.pnt = tuple(0.0 for i in range(3))
+            self.box1 = tuple(0.0 for i in range(3))
+            self.box2 = tuple(0.0 for i in range(3))
+            self.cent = tuple(0.0 for i in range(3))
+            self.height = 0.0
+            self.radius = 0.0
+            self.unknown2 = 0.0
+            self.volume = 0.0
+
+            self.materialDefinitionCount = 1
+            self.meshDescriptionCount = 1
+            self.boneCount = 0
+            self.entityCount = 0
+            self.unknownCount1 = 0
+            self.collisionPointCount = 0
+            
+            self.unknowns2 = tuple(0.0 for i in range(5))
+
+            self.vertexCount = 0
+            self.strideCount = 1
+            self.type = 2
+            self.indexCount = 0
+            self.modelCleaveCount = 0
     
     def write(self, writer:BufferedWriter):
         write_float(writer, self.unknown1)
@@ -241,6 +277,14 @@ class MaterialDefinition:
             self.normalMap = reader.read(64).decode("utf-8").replace("\0","")
             self.type = reader.read(64).decode("utf-8").replace("\0","")
             return
+        else:
+            self.diffuseMap = ""
+            self.lightMap = ""
+            self.growthMap = ""
+            self.specularMap = ""
+            self.animatedTexture = ""
+            self.normalMap = ""
+            self.type = ""
 
     def write(self, writer:BufferedWriter):
         write_str(writer, self.diffuseMap, 64)
@@ -283,6 +327,30 @@ class MeshDescription:
             self.materialRefs: List[MaterialRef] = []
 
             return
+        else:
+            self.facesCount = 0
+            self.indiciesOffset = 0
+            self.indiciesSize = 0
+            self.vertexOffset = 0
+            self.vertexSize = 0
+
+            self.axis1 = (0.0, 0.0, 0.0)
+            self.axis2 = (0.0, 0.0, 0.0)
+            self.axis3 = (0.0, 0.0, 0.0)
+            self.position = [0.0 for i in range(3)]
+            
+            self.unknowns1 = [0.0 for i in range(4)]
+            self.box1 = [0.0 for i in range(3)]
+            self.box2 = [0.0 for i in range(3)]
+            self.unknowns2 = [0.0 for i in range(5) ]
+            self.unknown_int = 0
+            self.bbox_volume = 0.0
+            self.materialRefsCount = 1
+            self.u2 = 0
+            self.id = 1
+            self.name = ''
+            self.unknowns3 = [0.0 for i in range(2)]
+            self.materialRefs: List[MaterialRef] = []
 
     def write(self, writer:BufferedWriter = None):
         write_int32(writer, self.facesCount)
@@ -324,6 +392,15 @@ class MaterialRef:
             self.facesSize = read_int32(reader)
             self.unknown = read_float(reader)
             return
+        else:
+            self.materialDefinition = 0
+            self.indiciesOffset = 0
+            self.indiciesSize = 0
+            self.vertexOffset = 0
+            self.vertexSize = 0
+            self.facesOffset = 0
+            self.facesSize = 0
+            self.unknown = 0.0
 
     def write(self, writer:BufferedWriter = None):
         write_int32(writer, self.materialDefinition)
@@ -367,6 +444,12 @@ class Entity:
             self.position = (read_float(reader), read_float(reader), read_float(reader))
             self.name = reader.read(256).decode("utf-8").replace("\0","")
             return
+        else:
+            self.axis1 = (0.0, 0.0, 0.0)
+            self.axis2 = (0.0, 0.0, 0.0)
+            self.axis3 = (0.0, 0.0, 0.0)
+            self.position = (0.0, 0.0, 0.0)
+            self.name = ""
 
     def write(self, writer:BufferedWriter = None):
         write_vector(writer, self.axis1, write_float)
@@ -384,6 +467,8 @@ class Unknown1:
         if reader:
             self.unknown = struct.unpack("<fff", reader.read(12))
             return
+        else:
+            self.unknown = (0.0, 0.0, 0.0)
 
     def write(self, writer:BufferedWriter = None):
         write_vector(writer, self.unknown, write_float)
@@ -397,6 +482,8 @@ class collisionPoint:
         if reader:
             self.unknown = struct.unpack("<fff", reader.read(12))
             return
+        else:
+            self.unknown = (0.0, 0.0, 0.0)
 
     def write(self, writer:BufferedWriter = None):
         write_vector(writer, self.unknown, write_float)
@@ -408,8 +495,8 @@ class Stride:
 
     def __init__(self, reader: BufferedReader = None):
         stride_format = [4, 8, 12, 4, 1]
+        size = 0x88
         if reader:
-            size = 0x88
             self.count = read_int32(reader)
             self.idSizes = [
                 (read_int32(reader), read_int32(reader)) for i in range(self.count)
@@ -420,6 +507,12 @@ class Stride:
             size = 0x88 - 4 - (8 * self.count)
             self.unknown = reader.read(size)
             return
+        else:
+            self.count = 0
+            self.idSizes = []
+            self.stride = 0
+            self.size = 0x88 - 4
+            self.unknown = bytes([0 for i in range(size)])
 
     def read_data(self, reader: BufferedReader):
         return reader.read(self.stride)
@@ -454,6 +547,10 @@ class Vertex:
                 if strideId == 2:
                     self.uvs.append((read_float(reader), read_float(reader)))
             return
+        else:
+            self.uvs = []
+            self.position = (0.0, 0.0, 0.0)
+            self.normal = (0.0, 0.0, 0.0)
 
     def write(self, writer: BufferedWriter):
         write_vector(writer, self.position, write_float)
@@ -464,7 +561,7 @@ class Vertex:
 def main():
     for filepath in glob("G:\\Lionhead Studios\\Black & White 2\\Data\\Art\\models\\m_greekaltar.bwm"):
         with open(filepath, "rb") as testBWM:
-            file = BWMFile(testBWM)
+            file = BWMFile()
             file.write("./m_greekaltar.bwm")
     return
 
