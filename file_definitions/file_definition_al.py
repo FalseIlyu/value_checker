6cbdcb3597bb685aa03fb96e615c1420b0ae3e40
FalseIlyu <ilyulemail@gmail.com>
2022-01-27 22:05:00 +0100

    Changing file organisation

--- file_definitions/file_definition_al.py
+++ file_definitions/file_definition_al.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 
+from asyncore import read
 from glob import glob
 from io import BufferedReader
+from matplotlib.pyplot import bone
 
 from numpy import single
 from file_definition_utilities import (
@@ -15,9 +17,8 @@ class AlFile:
     def __init__(self, reader: BufferedReader) -> None:
         if reader:
             self.header = AlHeader(reader)
-            self.bones = [AlBone(reader) for i in range(self.header.boneCount)]
-            reader.read((self.header.animstart + 0x5C)- (0x68 + (self.header.boneCount * 0x24)))
-            self.animation = [Animation(reader) for i in range(self.header.animationCount+1)]
+            self.skeleton = Skeleton(reader, self.header.animationMetadataOffset)
+            self.animation = [AnimationMetadata(reader) for i in range(self.header.animationMetadataCount)]
             return
         else:
             raise ValueError("Need a valid BufferedReader")
@@ -25,20 +26,30 @@ class AlFile:
 class AlHeader:
     """
     Header for binary animation files (.al)
-    Size : 0x68
+    Size : 0x60
     """
     def __init__(self, reader: BufferedReader) -> None:
         if reader:
             self.magicnumber1 = read_int32(reader)
             self.magicnumber2 = read_int32(reader)
             self.name = reader.read(0x40).decode("utf-8").replace("\0", "")
-            self.animstart = read_int32(reader)
+            self.animationMetadataOffset = read_int32(reader)
             self.size = read_int32(reader)
-            self.unknown2 = read_int32(reader)
-            self.animationCount = read_int32(reader)
+            self.animationDataOffset = read_int32(reader)
+            self.animationMetadataCount = read_int32(reader)
             self.unknowns2 = [read_int32(reader) for i in range(2)]
+            return
+        else:
+            raise ValueError("Need a valid BufferedReader")
+
+class Skeleton:
+    def __init__(self, reader: BufferedReader, sectionEnd: int) -> None:
+        if reader:
             self.boneCount = read_int32(reader)
-            self.buffer = read_int32(reader)
+            self.unknown = read_int32(reader)
+            self.bones = [AlBone(reader) for i in range(self.boneCount)]
+            bufferSize =  sectionEnd - (8 + (self.boneCount * 0x24))
+            self.buffer = reader.read(bufferSize)
             return
         else:
             raise ValueError("Need a valid BufferedReader")
@@ -55,17 +66,18 @@ class AlBone:
         else:
             raise ValueError("Need a valid BufferedReader")
 
-class Animation:
+class AnimationMetadata:
     """
     Maybe metadata on an animation
     Size : 0x94
     """
     def __init__(self, reader: BufferedReader) -> None:
         if reader:
-            self.unknown1 = read_int32(reader, signed=True)
             self.unknowns1 = [read_int32(reader) for i in range(2)]
             self.name = reader.read(0x40).decode("utf-8").replace("\0", "")
-            self.unknowns2 = [read_int32(reader,signed=True) for i in range(18)]
+            self.unknowns2 = [read_int32(reader) for i in range(7)]
+            self.unknowns3 = [read_int32(reader) for i in range(11)]
+            self.animationOffset = read_int32(reader)
         else:
             raise ValueError("Need a valid BufferedReader")
         pass
