9f18904b9765cb52fda7cc761eb40cec1b1af22e
FalseIlyu <ilyulemail@gmail.com>
2022-04-14 14:36:08 +0200

    Adding information on frames and duration of animation (.al)

--- file_definitions/file_definition_al.py
+++ file_definitions/file_definition_al.py
@@ -9,32 +9,36 @@ from .file_definition_utilities import (
     read_int32
 )
 
+
 class AlFile:
 
     def __init__(self, reader: BufferedReader) -> None:
         if reader:
             self.header = AlHeader(reader)
-            self.skeleton = Skeleton(reader, self.header.animationMetadataOffset)
+            self.skeleton = Skeleton(
+                reader, self.header.animationMetadataOffset
+                )
             self.animationMetadataArray = [
-                AnimationMetadata(reader) 
+                AnimationMetadata(reader)
                 for i in range(self.header.animationCount)
                 ]
             size = [
-                self.animationMetadataArray[i+1].animationOffset 
-                for i in range(self.header.animationCount - 1) 
+                self.animationMetadataArray[i+1].animationOffset
+                for i in range(self.header.animationCount - 1)
                 ]
             size.append(self.header.size + 0x60)
             size = [
-                size[i] - self.animationMetadataArray[i].animationOffset 
+                size[i] - self.animationMetadataArray[i].animationOffset
                 for i in range(len(size))
                 ]
             self.animationDataArray = [
-                AnimationData(reader, size[i]) 
+                AnimationData(reader, size[i])
                 for i in range(self.header.animationCount)
                 ]
             return
         else:
             raise ValueError("Need a valid BufferedReader")
+
 
 class AlHeader:
     """
@@ -50,10 +54,11 @@ class AlHeader:
             self.size = read_int32(reader)
             self.animationDataOffset = read_int32(reader)
             self.animationCount = read_int32(reader)
-            self.unknowns2 = [read_int32(reader) for i in range(2)]
+            self.unknowns2 = [read_float(reader) for i in range(2)]
             return
         else:
             raise ValueError("Need a valid BufferedReader")
+
 
 class Skeleton:
     def __init__(self, reader: BufferedReader, sectionEnd: int) -> None:
@@ -61,11 +66,12 @@ class Skeleton:
             self.boneCount = read_int32(reader)
             self.unknown = read_int32(reader)
             self.bones = [AlBone(reader) for i in range(self.boneCount)]
-            bufferSize =  sectionEnd - (8 + (self.boneCount * 0x24))
+            bufferSize = sectionEnd - (8 + (self.boneCount * 0x24))
             self.buffer = reader.read(bufferSize)
             return
         else:
             raise ValueError("Need a valid BufferedReader")
+
 
 class AlBone:
     """
@@ -80,6 +86,7 @@ class AlBone:
         else:
             raise ValueError("Need a valid BufferedReader")
 
+
 class AnimationMetadata:
     """
     Maybe metadata of an animation
@@ -91,11 +98,17 @@ class AnimationMetadata:
             self.name = reader.read(0x40).decode("utf-8").replace("\0", "")
             self.unknowns2 = [read_int32(reader) for i in range(6)]
             self.boneCount = read_int32(reader)
-            self.unknowns3 = [read_int32(reader) for i in range(11)]
+            self.frameCount = read_int32(reader)
+            self.samplingRate = read_float(reader)
+            self.duration = read_float(reader)
+            self.unknown2 = read_int32(reader)
+            self.unknown3 = read_int32(reader)
+            self.unknowns5 = [read_float(reader) for i in range(6)]
             self.animationOffset = read_int32(reader)
             return
         else:
             raise ValueError("Need a valid BufferedReader")
+
 
 class AnimationData:
     """
@@ -106,23 +119,31 @@ class AnimationData:
         if reader:
             self.unknowns1 = [read_int32(reader) for i in range(2)]
             self.name = reader.read(0x40).decode("utf-8").replace("\0", "")
-            self.unknown1 = read_int32(reader)
-            self.unknowns2 = [read_int32(reader) for i in range(5)]
+            # self.unknown1 = read_int32(reader)
+            self.unknowns2 = [read_int32(reader) for i in range(6)]
             self.boneCount = read_int32(reader)
-            self.unknowns3 = [read_int32(reader) for i in range(11)]
-            self.size = size - 0x90 - self.unknowns2[2]
-            self.unknownsData = reader.read(size - 0x90)
+            self.frameCount = read_int32(reader)
+            self.samplingRate = read_float(reader)
+            self.duration = read_float(reader)
+            self.unknown2 = read_int32(reader)
+            self.unknown3 = read_int32(reader)
+            self.unknowns5 = [read_float(reader) for i in range(6)]
+            self.size = size - 0x90
+            self.unknownsData = reader.read(self.size)
             return
         else:
             raise ValueError("Need a valid BufferedReader")
 
+
 def main():
-    for filepath in glob("G:\\Lionhead Studios\\Black & White 2\\Data\\Art\\**\\**.al"):
+    for filepath in glob(
+        "G:\\Lionhead Studios\\Black & White 2\\Data\\Art\\**\\catapult.al"
+    ):
         with open(filepath, "rb") as alTest:
             file = AlFile(alTest)
             continue
     return
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     main()
